aa755291fde0ccaf4cd9bda9e02703182665e595
Florent Murat <florentmurat@ymail.com>
2026-03-17 20:24:17 +0100

    - Transformation de l'expérience utilisateur sur la page d'accueil avec des modales détaillées, une section "Univers" interactive et une navigation par scroll spy. - Amélioration de la profondeur narrative des projets et de l'identité visuelle "Light Developer".

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+export const collections = {
+	work: defineCollection({
+		loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/work" }),
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+			href: z.string().optional(),
+			status: z.enum(['in_progress', 'finish']).optional(),
+		}),
+	}),
+};
